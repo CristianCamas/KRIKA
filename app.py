92a78ff2ff62e76e8705ce0e08258b794eaa8abd
CristianCamas <59580875+CristianCamas@users.noreply.github.com>
2020-01-13 09:26:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ class myHandler(BaseHTTPRequestHandler):
 				try:
 					self.wfile.write('hello')
 				except:
-					self.wfile.write(bytes('hola crack, mastodonte, titan eres un puto amo...Saludos', 'UTF-8'))
+					self.wfile.write(bytes('hola crack, mastodonte, titan eres un puto amo...Saludos Crack', 'UTF-8'))
 				
 			return
 
